51120fa74a977fe9bb30162e758df2da4456fbf1
Segebre <rsegebre@colgate.edu>
2014-04-05 10:25:25 -0400

    Conf file and test post

---
+++ pelican.conf.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = u'Roberto Segebre'
+DISQUS_SITENAME = 'rsegebre'
+SITENAME = u"foodie.rsegebre"
+SITEURL = 'http://foodie.rsegebre.com'
+#SITELOGO = 'static/images/favicon.ico'
+
+TIMEZONE = "America/New_York"
+LOCALE = ""
+
+FEED_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feed/%s.rss.xml'
+GITHUB_URL = 'http://github.com/rsegebre'
+#GOOGLE_ANALYTICS = 'UA-32786140-1'
+DEFAULT_LANG='en'
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_CATEGORIES_ON_SIDEBAR = True
+#RELATIVE_URLS = True
+
+#Theme
+THEME = '/Users/rsegebre/Documents/Projects/theme/pelican-elegant-1.3'
+BOOTSTRAP_THEME = 'simplex'
+
+# Blogroll
+LINKS =  (
+    ('Colgate COSC Dept. Website', 'http://cs.colgate.edu'),
+    ('Coding Horror Blog by Jeff Atwood', 'http://www.codinghorror.com/blog/')
+         )
+
+# Social widget
+SOCIAL = (
+          ('twitter', 'http://twitter.com/rss1989'),
+          ('github', 'http://github.com/rsegebre'),
+          ('facebook', 'http://www.facebook.com/rsegebre'),
+          ('linkedIn',
+'http://www.linkedin.com/pub/roberto-segebre/40/8b1/6b2'),
+         )
+TWITTER_USERNAME = 'rss1989'
+ADDTHIS_PROFILE = 'ra-52d5e9e5415eda76'
+
+DEFAULT_PAGINATION = 5
---
+++ posts/2014-04-05-testing-format.rst
@@ -0,0 +1,10 @@
+Testing website theme:
+#######################
+
+
+:date: 2014-04-05
+:author: Roberto Segebre
+:tags: test
+:category: Recipes
+
+I just want to test this theme!
