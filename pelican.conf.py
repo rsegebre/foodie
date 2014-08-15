621bd03db8e2d559301b77b47e9a4008664f39ab
Segebre <rsegebre@colgate.edu>
2014-08-15 01:13:29 -0400

    added about landing section

--- pelican.conf.py
+++ pelican.conf.py
@@ -68,3 +68,21 @@ TWITTER_USERNAME = 'rss1989'
 ADDTHIS_PROFILE = 'ra-52d5e9e5415eda76'
 
 DEFAULT_PAGINATION = 5
+
+LANDING_PAGE_ABOUT = {'title': 'Food with little bit of software on the side',
+        'details': """
+ Welcome to foodie.rsegebre.com, a place to talk share content about (1)
+recipes, (2) document any food experiences around Boston and neighboring areas,
+(3) figure out ways in which technology can innovate cooking and (4)
+criticize/try-out recipes that I find online and see how feasable they are.
+
+This will get updated later on with more details... For the time being this
+will have some of my to-do tasks for this website.
+
+TODO: * Fix tiptue searching * Generate sitemap * Create Pages for each of the
+4 purposes of this website * Figure out the best way to include images in
+recipes.
+
+If you have any questons feel free to use the comment box below.
+
+~rs"""}
